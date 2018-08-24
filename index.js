c3fb39057b8092e4896b9d59f1228be99ad7c2f8
dougdev5 <42444199+dougdev5@users.noreply.github.com>
2018-08-23 22:11:04 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,78 @@
+const Discord = require("discord.js");
+const YTDL = require("ytdl-core");
+
+const TOKEN = "NDgyMzI1OTA0MTUyODU0NTMw.DmDR5g.l0zMXHqVfJnaEveMA1wvSkg96Yc";
+const PREFIX = "pm.";
+
+function play(connection, message) {
+    var server = servers[message.guild.id];
+
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+
+    server.queue.shift();
+
+    server.dispatcher.on("end", function() {
+        if (server.queue[0]) play(connection, message);
+        else connection.disconnect();
+    });
+}
+
+var bot = new Discord.Client();
+
+var servers = {};
+
+bot.on("ready", function() {
+    bot.user.setActivity("pm.ajuda", {type: "STREAMING", url: "https://www.twitch.tv/doug_dev"});
+});
+
+bot.on("message", function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if (!message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIX.length).split(" ");
+
+    switch (args[0].toLowerCase()) {
+        case "ajuda":
+            message.channel.send("```Portal Música\n\nComandos:\npm.ouvir <link>\npm.pular\npm.parar```");
+        break;
+        case "ouvir":
+            if (!args[1]) {
+                message.channel.send("Por-favor coloque um link!");
+                return;
+            }
+
+            if (!message.member.voiceChannel) {
+                message.channel.send("Você deve está em um canal de Musica!");
+                return;
+            }
+
+            if (!servers[message.guild.id]) servers[message.guild.id] = {
+                queue: []
+            };
+
+            var server = servers[message.guild.id];
+
+            server.queue.push(args[1]);
+
+            if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+                play(connection, message);
+            });
+        break;
+        case "pular":
+            var server = servers[message.guild.id];
+
+            if (server.dispatcher) server.dispatcher.end();
+        break;
+        case "parar":
+            var server = servers[message.guild.id];
+
+            if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+        break;
+        default:
+            message.channel.send(":heavy_multiplication_x: Comando não encontrado!");
+        break;
+    }
+});
+
+bot.login(process.env.TOKEN);
